1a501f2e0128e6a8759bcb6f7ba07e5fe16557bf
Якоб Месенгисер <anatolkabasurman@gmail.com>
2020-11-22 14:18:51 +0300

    Fixed small issues

--- engines/CoordsCalculation.psm1
+++ engines/CoordsCalculation.psm1
@@ -6,7 +6,7 @@ function CalculateCoords {
     )
     [System.Double]$Latitude = $InitCoords['latitude']
     [System.Double]$Longtitude = $InitCoords['longtitude']
-    [System.String[]]$StringCoords = "$Latitude $Longtitude $InitAngle $DistanceToGo" | .\engines\dir.exe | % {$_ -split [System.Text.Encoding]::ASCII.GetString(9)} 
+    [System.String[]]$StringCoords = "$Latitude $Longtitude $InitAngle $DistanceToGo" | ./engines/Direct.exe | % {$_ -split [System.Text.Encoding]::ASCII.GetString(9)} 
     [hashtable] $ResultingCoords = @{'latitude'=($StringCoords[0] -as [double]); 'longtitude'=($StringCoords[1] -as [double])}
 	return $ResultingCoords;
 }
