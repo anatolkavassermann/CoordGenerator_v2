c1dc2eb9f13b99c8b205162424eec327c5036940
Якоб Месенгисер <anatolkabasurman@gmail.com>
2020-11-27 22:03:22 +0300

    New version of WriteToExcel

--- WriteToExcel.ps1
+++ WriteToExcel.ps1
@@ -9,35 +9,36 @@ switch ([System.Environment]::OSVersion.VersionString -match "Unix") {
         exit;
     }
 }
-
 $DataForExcel = [System.Collections.ArrayList]::new()
 $i = 0
 $TimeShift = 0
-$OutputConfigFile = gc "./conf/Output.txt"
-$OutputConfigFile | % {
-    if (($TimeShift -gt $finish) -and ($finish -ne 0)) {
-        return;
-    }
-    $t = 0
-    $s = [System.Int32]::TryParse($_.Substring(1,$_.IndexOf(">")-1), [ref] $t)
-    if ($s -eq $true) {
-        if ($t -ge $TimeShift) {
-            $Matchess = $OutputConfigFile | Select-String $_
-            switch ($Matchess.Matches.Count -eq 1) {
-                $true {
-                    $k = $Matchess.LineNumber
-                    break;
-                }
-                $false {
-                    $k = $Matchess.LineNumber[$Matchess.LineNumber.Count-1]
-                    break;
-                }
+$OutputConfigFile = gc "./conf/Output1.txt"
+$lineCount = 0
+$t = 0
+while ($lineCount -lt $OutputConfigFile.Count-1) {
+    [void] [System.Int32]::TryParse($OutputConfigFile[$lineCount].Substring(1,$OutputConfigFile[$lineCount].IndexOf(">")-1), [ref] $t)
+    if ($t -lt $TimeShift) {
+        while ($t -lt $TimeShift) {
+            try {
+                [void] [System.Int32]::TryParse($OutputConfigFile[$lineCount].Substring(1,$OutputConfigFile[$lineCount].IndexOf(">")-1), [ref] $t)
             }
-            $Count = (($OutputConfigFile[$i..$k] | foreach {$_ -split ";" | select -Index 3}) | sort -Unique).Count
-            [void] $DataForExcel.Add(($TimeShift,$Count))
-            $i = $k
-            $TimeShift += $shift
+            catch {
+                break;
+            }
+            $lineCount++
         }
+        $lineCount--
+        $Cnt = (($OutputConfigFile[$i..($lineCount-1)] | foreach {$_ -split ";" | select -Index 3}) | sort -Unique).Count
+        [void] $DataForExcel.Add(($TimeShift, $Cnt))
+        $i = $lineCount-1
+        $TimeShift += $shift
+    }
+    if (($t -gt $finish) -and ($finish -ne 0)) {
+        break;
+    }
+    if ($t -ge $TimeShift) {
+        [void] $DataForExcel.Add(($TimeShift,0))
+        $TimeShift += $shift
     }
 }
 switch ($finish) {
@@ -56,11 +57,11 @@ $xl.Visible = $true
 $OutputData | Set-Clipboard
 [void] $ws.Range("A1").Select()
 $ws.paste()
-[void] $ws.UsedRange.Columns.AutoFit()
 $r = $ws.UsedRange
 $tr = $xl.WorksheetFunction.Transpose($r.Value2)
 $r.Delete()
 $xl.ActiveSheet.Range("A1").Resize($tr.GetUpperBound(0), $tr.GetUpperBound(1)) = $tr
+[void] $ws.UsedRange.Columns.AutoFit()
 $Chart = $ws.Shapes.AddChart().Chart
 $Chart.Parent.Height = 280
 $Chart.Parent.Width = 500
